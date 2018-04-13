8a1bbd627e042152a33dc65f58d3f4b65cf060e2
adirksen <anthonyhdirksen@gmail.com>
2018-04-12 20:05:44 -0600

    Added getters/setters + some extra fields to the Aiport and Flight classes

--- src/assignment11/Airport.java
+++ src/assignment11/Airport.java
@@ -3,32 +3,117 @@ package assignment11;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a vertex when finding the best flight path
+ */
 public class Airport {
-    private List<Flight> flights = new ArrayList<>();
-    private String airliner;
-    private String location;
 
-    public String getAirliner() {
-        return airliner;
-    }
+  private List<Flight> flights = new ArrayList<>();
 
-    public void setAirliner(String airliner) {
-        this.airliner = airliner;
-    }
+  private String airliner, location;
 
-    public List<Flight> getFlights() {
-        return flights;
-    }
+  private double cost, time, cancelled;
 
-    public void setFlights(List<Flight> flights) {
-        this.flights = flights;
-    }
+  private int delay, distance;
 
-    public String getLocation() {
-        return location;
-    }
+  private boolean visited;
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+  private Airport previous;
+
+  private FlightCriteria criteria;
+
+
+  /*--- FIELD SETTERS ---*/
+  
+  public void setAirliner(String airliner) {
+    this.airliner = airliner;
+  }
+
+  public void setFlights(List<Flight> flights) {
+    this.flights = flights;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public void setDelay(int delay) {
+    this.delay = delay;
+  }
+
+  public void setCancelled(double cancelled) {
+    this.cancelled = cancelled;
+  }
+
+  public void setTime(double time) {
+    this.time = time;
+  }
+
+  public void setCost(double cost) {
+    this.cost = cost;
+  }
+
+  public void setPrevious(Airport airport) {
+    this.previous = airport;
+  }
+
+  public void setVisited(boolean state) {
+    this.visited = state;
+  }
+
+  public void setCriteria(FlightCriteria criteria) {
+    this.criteria = criteria;
+  }
+
+  public void setDistance(int distance) {
+    this.distance = distance;
+  }
+
+  /*--- FIELD GETTERS ---*/
+
+  public String getAirliner() {
+    return airliner;
+  }
+
+  public List<Flight> getFlights() {
+    return flights;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public int getDelay() {
+    return this.delay;
+  }
+
+  public double getCancelled() {
+    return this.cancelled;
+  }
+
+  public double getTime() {
+    return this.time;
+  }
+
+  public double getCost() {
+    return this.cost;
+  }
+
+  public Airport getPrevious() {
+    return this.previous;
+  }
+
+  public boolean getVisited() {
+    return this.visited;
+  }
+
+  public FlightCriteria getCriteria() {
+    return this.criteria;
+  }
+
+  public int getDistance() {
+    return this.distance;
+  }
+
+
 }
--- src/assignment11/Flight.java
+++ src/assignment11/Flight.java
@@ -1,66 +1,106 @@
 package assignment11;
 
+/**
+ * Represents an edge when finding the best flight path
+ */
 public class Flight {
-    private String origin, destination, carrier, delay, cancel, time, distance, cost, airliner;
-    private double edgeWeight;
+
+  private String origin, destination, carrier, delay, cancel, time, distance, cost, airliner;
+  private double edgeWeight;
 
 
-    public double getEdgeWeight(FlightCriteria criteria) {
-        switch (criteria) {
-            case TIME:
-                edgeWeight = Double.parseDouble(time);
-                break;
-            case DISTANCE:
-                edgeWeight = Double.parseDouble(distance);
-                break;
-            case DELAY:
-                edgeWeight = Double.parseDouble(delay);
-                break;
-            case CANCELED:
-                edgeWeight = Double.parseDouble(cancel);
-                break;
-        }
-        return edgeWeight;
+  public double getEdgeWeight(FlightCriteria criteria) {
+    switch (criteria) {
+      case TIME:
+        edgeWeight = Double.parseDouble(time);
+        break;
+      case DISTANCE:
+        edgeWeight = Double.parseDouble(distance);
+        break;
+      case DELAY:
+        edgeWeight = Double.parseDouble(delay);
+        break;
+      case CANCELED:
+        edgeWeight = Double.parseDouble(cancel);
+        break;
     }
-    public void setEdgeWeight(float edgeWeight) {
-        this.edgeWeight = edgeWeight;
-    }
+    return edgeWeight;
+  }
+
+  public void setEdgeWeight(float edgeWeight) {
+    this.edgeWeight = edgeWeight;
+  }
 
 
-    //-----FIELD SETTERS---------//
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
+  /*--- FIELD SETTERS ---*/
+  
+  public void setOrigin(String origin) {
+    this.origin = origin;
+  }
 
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
 
-    public void setCarrier(String carrier) {
-        this.carrier = carrier;
-    }
+  public void setCarrier(String carrier) {
+    this.carrier = carrier;
+  }
 
-    public void setDelay(String delay) {
-        this.delay = delay;
-    }
+  public void setDelay(String delay) {
+    this.delay = delay;
+  }
 
-    public void setCancel(String cancel) {
-        this.cancel = cancel;
-    }
+  public void setCancel(String cancel) {
+    this.cancel = cancel;
+  }
 
-    public void setTime(String time) {
-        this.time = time;
-    }
+  public void setTime(String time) {
+    this.time = time;
+  }
 
-    public void setDistance(String distance) {
-        this.distance = distance;
-    }
+  public void setDistance(String distance) {
+    this.distance = distance;
+  }
 
-    public void setCost(String cost) {
-        this.cost = cost;
-    }
+  public void setCost(String cost) {
+    this.cost = cost;
+  }
 
-    public void setAirliner(String airliner) {
-        this.airliner = airliner;
-    }
+  public void setAirliner(String airliner) {
+    this.airliner = airliner;
+  }
+
+  /*--- FIELD GETTERS ---*/
+
+  public String getOrigin() {
+    return this.origin;
+  }
+
+  public String getDestination() {
+    return this.destination;
+  }
+
+  public String getDelay() {
+    return this.delay;
+  }
+
+  public String getCancel() {
+    return this.cancel;
+  }
+
+  public String getTime() {
+    return this.time;
+  }
+
+  public String getDistance() {
+    return this.distance;
+  }
+
+  public String getCost() {
+    return this.cost;
+  }
+
+  public String getAirliner() {
+    return this.airliner;
+  }
 }
